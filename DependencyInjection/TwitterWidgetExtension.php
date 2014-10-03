b831c9b283eaaf0ea00b36d7d29c082759c7e69e
mpalourdio <mpalourdio@gmail.com>
2014-10-03 15:14:14 +0200

    First working copy. Tests and Ci needed

---
+++ DependencyInjection/TwitterWidgetExtension.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace TwitterWidgetBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class TwitterWidgetExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ Extension/TimelineWidget.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace TwitterWidgetBundle\Extension;
+
+use TwitterWidgets\Options\WidgetOptions;
+use TwitterWidgets\Timeline\TimelineBuilderInterface;
+
+class TimelineWidget extends \Twig_Extension
+{
+    protected $widgetOptions;
+    protected $timeline;
+
+    /**
+     * @param WidgetOptions            $widgetOptions
+     * @param TimelineBuilderInterface $timeline
+     */
+    public function __construct(WidgetOptions $widgetOptions, TimelineBuilderInterface $timeline)
+    {
+        $this->widgetOptions = $widgetOptions;
+        $this->timeline      = $timeline;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            'tw' => new \Twig_Function_Method($this, 'renderWidget', ['is_safe' => ['html']])
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'timeline';
+    }
+
+    /**
+     * @param  array $options
+     * @param  bool  $addJs
+     * @return string
+     */
+    public function renderWidget($options, $addJs = true)
+    {
+        $this->widgetOptions->setFromArray($options);
+
+        return $this->timeline->renderWidget($addJs);
+    }
+}
